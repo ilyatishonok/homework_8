65eecc888533c2df90e1a32646413a72165a52a9
Ilya <tishonok.ilya@ya.ru>
2018-12-20 16:25:18 +0300

    Updates

---
+++ precache-manifest.4292ac79d9ff0046e5524cb22958e98c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "911dd5dcad774528acf2",
+    "url": "/static/js/main.911dd5dc.chunk.js"
+  },
+  {
+    "revision": "597a0689fd8cd466b392",
+    "url": "/static/js/1.597a0689.chunk.js"
+  },
+  {
+    "revision": "53eade824d63fba30d26ad5f186f69eb",
+    "url": "/index.html"
+  }
+];
